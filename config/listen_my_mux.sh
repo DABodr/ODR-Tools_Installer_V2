932b6901c02eb76bbfe1cc7a4ef9ba1d06d8b4be
RSAP <46652797+DABodr@users.noreply.github.com>
2020-02-25 19:14:38 +0100

    Add files via upload

---
+++ config/listen_my_mux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/$USER/dab/mmbtools-aux/zmqtest/zmq-sub/zmq-sub localhost 18081 | /home/$USER/dab/dablin/build/src/dablin_gtk -s 0x4DB0
+
+# DABlin will start with MAXXIMA ( SID 0x4DB0 )
